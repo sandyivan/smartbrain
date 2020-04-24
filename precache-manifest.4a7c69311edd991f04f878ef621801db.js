6fa76c5ad8ff2c46a5005faa5614430fbcb0ff37
sandeepivan <zandalba@gmail.com>
2020-04-25 02:08:10 +0800

    Updates

---
+++ precache-manifest.4a7c69311edd991f04f878ef621801db.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "214b1a1f32a906f0328bbaecc4a8c7fa",
+    "url": "/smartbrain/index.html"
+  },
+  {
+    "revision": "ddfe7be1e53de1634801",
+    "url": "/smartbrain/static/css/2.66b7f74f.chunk.css"
+  },
+  {
+    "revision": "f8212ce01f78d8a0fde4",
+    "url": "/smartbrain/static/css/main.59022bf8.chunk.css"
+  },
+  {
+    "revision": "ddfe7be1e53de1634801",
+    "url": "/smartbrain/static/js/2.961559d3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/smartbrain/static/js/2.961559d3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f8212ce01f78d8a0fde4",
+    "url": "/smartbrain/static/js/main.42033220.chunk.js"
+  },
+  {
+    "revision": "be86346f47316a9fda85",
+    "url": "/smartbrain/static/js/runtime-main.bb56a74b.js"
+  }
+]);
